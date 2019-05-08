fd7d396f2ff0ea339a0e18cad004611c135aa196
Evgeny <sukhanovevgeniy96@gmail.com>
2019-05-08 21:09:28 +0300

    Created ui for the game

---
+++ Puzzle2048/Core/Extensions/UIViewExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtensions.swift
+//  Puzzle2048
+//
+//  Created by Евгений Суханов on 08/05/2019.
+//  Copyright © 2019 Евгений Суханов. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
